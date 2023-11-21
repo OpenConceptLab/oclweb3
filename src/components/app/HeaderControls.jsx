8934cd182c4ec0c00d58d04af6b1664301b46733
Sny <sny.aggarwal@gmail.com>
2023-11-21 12:11:09 +0530

    Styling dashboard header and added create repo button

--- src/components/app/HeaderControls.jsx
+++ src/components/app/HeaderControls.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { useTranslation } from 'react-i18next'
 import PersonIcon from '@mui/icons-material/Face2';
 import IconButton from '@mui/material/IconButton';
-import Chip from '@mui/material/Chip';
 import { getLoginURL, isLoggedIn } from '../../common/utils';
 import Languages from './Languages';
+import Button from '../common/Button';
 
 const HeaderControls = () => {
   const { t } = useTranslation()
@@ -16,7 +16,7 @@ const HeaderControls = () => {
           <IconButton color='primary'>
             <PersonIcon fontSize='inherit'/>
           </IconButton>:
-        <Chip label={t('auth.sign_in')} color='primary' style={{height: '40px', borderRadius: '100px', marginLeft: '8px'}} href={getLoginURL()} component='a' />
+        <Button className='default-button-styles' label={t('auth.sign_in')} color='primary' style={{marginLeft: '8px'}} href={getLoginURL()} component='a' />
       }
     </div>
   )
