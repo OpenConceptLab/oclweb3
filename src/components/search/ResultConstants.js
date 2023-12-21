047c5fa16d1b341e53ecbe5a8efd2015728f8c1b
Sny <sny.aggarwal@gmail.com>
2023-12-21 17:31:05 +0530

    OpenConceptLab/ocl_issues#1703 | concept retired changes

--- src/components/search/ResultConstants.js
+++ src/components/search/ResultConstants.js
@@ -11,7 +11,7 @@ import Retired from '../common/Retired';
 export const ALL_COLUMNS = {
   concepts: [
     {id: 'id', labelKey: 'common.id', value: 'id', sortOn: 'id_lowercase', className: 'searchable'},
-    {id: 'name', labelKey: 'concept.display_name', value: 'display_name', sortOn: '_name', className: 'searchable', sortBy: 'asc', renderer: item => (<span><React.Fragment>{item.display_name} {item.retired && <Retired style={{marginLeft: '8px'}}/>}</React.Fragment></span>)},
+    {id: 'name', labelKey: 'concept.display_name', value: 'display_name', sortOn: '_name', className: 'searchable', sortBy: 'asc', renderer: item => (<span><React.Fragment>{item.retired && <Retired style={{marginRight: '8px'}}/>} {item.display_name}</React.Fragment></span>)},
     {id: 'class', labelKey: 'concept.concept_class', value: 'concept_class', sortOn: 'concept_class'},
     {id: 'datatype', labelKey: 'concept.datatype', value: 'datatype', sortOn: 'datatype'},
     {id: 'updatedOn', labelKey: 'common.updated_on', value: 'version_updated_on', formatter: formatDate, sortOn: 'last_update', global: false},
