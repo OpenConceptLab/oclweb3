909df43e52d62e8f4da0c67ede93edff2412cc9d
Sny <sny.aggarwal@gmail.com>
2023-12-22 13:02:02 +0530

    OpenConceptLab/ocl_issues#1722 | Repo Home | url change should reflect in new fetch or 404

--- src/components/repos/RepoHome.jsx
+++ src/components/repos/RepoHome.jsx
@@ -8,11 +8,13 @@ import RepoTabs from './RepoTabs';
 import Search from '../search/Search';
 import { WHITE } from '../../common/constants';
 import ConceptHome from '../concepts/ConceptHome';
+import Error404 from '../errors/Error404';
 
 const RepoHome = () => {
   const location = useLocation()
 
   const [tab, setTab] = React.useState("concepts")
+  const [status, setStatus] = React.useState(false)
   const [repo, setRepo] = React.useState(false)
   const [loading, setLoading] = React.useState(true)
   const [showItem, setShowItem] = React.useState(false)
@@ -21,31 +23,44 @@ const RepoHome = () => {
   const getURL = () => (location.pathname + '/').replace('//', '/')
   const fetchRepo = () => {
     setLoading(true)
-    APIService.new().overrideURL(getURL()).get(null, null, {includeSummary: true}).then(response => {
+    APIService.new().overrideURL(getURL()).get(null, null, {includeSummary: true}, true).then(response => {
+      setStatus(response?.status || response?.response.status)
       setLoading(false)
-      setRepo(response?.data || {})
+      setRepo(response?.data || response?.response?.data || {})
     })
   }
 
   React.useEffect(() => {
     fetchRepo()
-  }, [])
+  }, [location.pathname])
 
   return (
     <div className='col-xs-12 padding-0' style={{overflow: 'auto', borderRadius: '8px'}}>
       <LoaderDialog open={loading} />
       <Paper component="div" className={showItem?.id ? 'col-xs-7 split padding-0' : 'col-xs-12 split padding-0'} sx={{backgroundColor: bgColor, borderRadius: '10px', height: '86vh', boxShadow: 'none', p: 0, border: 'solid 0.3px', borderColor: 'surface.n90'}}>
-        <RepoHeader repo={repo} />
-        <RepoTabs repo={repo} tab={tab} onChange={(event, newTab) => setTab(newTab)} />
-        <Search
-          resource={tab}
-          url={getURL() + 'concepts/'}
-          defaultFiltersOpen={false}
-          nested
-          noTabs
-          onShowItem={setShowItem}
-          showItem={showItem}
-        />
+        {
+          (repo?.id || loading) &&
+            <React.Fragment>
+              <RepoHeader repo={repo} />
+              <RepoTabs repo={repo} tab={tab} onChange={(event, newTab) => setTab(newTab)} />
+              {
+                repo?.id &&
+                  <Search
+                    resource={tab}
+                    url={getURL() + 'concepts/'}
+                    defaultFiltersOpen={false}
+                    nested
+                    noTabs
+                    onShowItem={setShowItem}
+                    showItem={showItem}
+                  />
+              }
+            </React.Fragment>
+        }
+        {
+          !loading && status === 404 &&
+            <Error404 />
+        }
       </Paper>
       <div className={'col-xs-5 padding-0' + (showItem ? ' split-appear' : '')} style={{marginLeft: '16px', width: showItem ? 'calc(41.66666667% - 16px)' : 0, backgroundColor: WHITE, borderRadius: '10px', height: showItem ? '86vh' : 0, opacity: showItem ? 1 : 0}}>
         {
