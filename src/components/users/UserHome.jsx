438abb7f2be9d677aaa5291c21b69f6deafa834b
Sny <sny.aggarwal@gmail.com>
2024-06-19 08:59:31 +0530

    OpenConceptLab/ocl_issues#1852 | other user profile can be accessed

--- src/components/users/UserHome.jsx
+++ src/components/users/UserHome.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom'
-import { getCurrentUser, canAccessUser } from '../../common/utils'
+import { getCurrentUser } from '../../common/utils'
 import UserProfile from './UserProfile'
 import UserRepositories from './UserRepositories';
 import APIService from '../../services/APIService'
-import Error403 from '../errors/Error403';
 
 
 const UserHome = () => {
   const [user, setUser] = React.useState({})
   const params = useParams()
   const height = 'calc(100vh - 100px)'
-  const canAccess = getCurrentUser()?.username && canAccessUser(params.user)
 
   const fetchUser = () => {
     const currentUser = getCurrentUser()
@@ -36,21 +34,15 @@ const UserHome = () => {
 
   return (
     <div className='col-xs-12 padding-0'>
-      {
-        canAccess ?
-          <React.Fragment>
-            <div className='col-xs-3' style={{height: height, padding: '24px 24px 24px 8px', maxWidth: '20%'}}>
-              <UserProfile user={user} />
-            </div>
-            <div className='col-xs-10 padding-0' style={{backgroundColor: '#FFF', borderRadius: '10px', height: height, maxWidth: '80%'}}>
-              {
-                user?.url &&
-                  <UserRepositories user={user} profile />
-              }
-            </div>
-          </React.Fragment> :
-        <Error403 />
-      }
+      <div className='col-xs-3' style={{height: height, padding: '24px 24px 24px 8px', maxWidth: '20%'}}>
+        <UserProfile user={user} />
+      </div>
+      <div className='col-xs-10 padding-0' style={{backgroundColor: '#FFF', borderRadius: '10px', height: height, maxWidth: '80%'}}>
+        {
+          user?.url &&
+            <UserRepositories user={user} profile />
+        }
+      </div>
     </div>
   )
 }
