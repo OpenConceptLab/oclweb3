47a268e5830646b960cfd3254872b829e2ed07ce
Sny <sny.aggarwal@gmail.com>
2024-08-21 18:58:04 +0530

    OpenConceptLab/ocl_issues#1780 | Events fixes

--- src/components/users/UserOverview.jsx
+++ src/components/users/UserOverview.jsx
@@ -18,14 +18,14 @@ const UserOverview = ({ user, bookmarks, events, height }) => {
       <div className='col-xs-9' style={{padding: '0 16px', height: '100%', overflow: 'auto', width: '80%'}}>
         <Bookmarks bookmarks={bookmarks} />
         {
-          Boolean(user?.url && repos == 0) &&
+          Boolean(events?.length) &&
+            <Events user={user} events={events} height={height} />
+        }
+        {
+          Boolean(user?.url && repos == 0 && events?.length === 0) &&
             <div className='col-xs-12' style={{marginTop: '15%', marginBottom: '16px'}}>
               <EmptyOverview label={`${user.name} ${t('user.user_has_not_created_public_repos_suffix')}`} />
             </div>
-        }
-        {
-          events?.length &&
-            <Events user={user} events={events} />
         }
       </div>
       <Paper className='col-xs-3' sx={{width: '20% !important', borderLeft: '0.5px solid', borderColor: 'surface.n90', borderRadius: 0, boxShadow: 'none', padding: '16px', height: '100%', overflow: 'auto', backgroundColor: 'default.main'}}>
