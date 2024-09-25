f3a1a143e5e8ef009b4af0ffd3c8fba1fc87280b
Sny <sny.aggarwal@gmail.com>
2024-09-25 13:18:05 +0530

    OpenConceptLab/ocl_issues#1861 | user profile image for logged out dashboard

--- src/components/users/UserIcon.jsx
+++ src/components/users/UserIcon.jsx
@@ -8,13 +8,15 @@ const UserIcon = ({ user, color, logoClassName, sx, authenticated }) => {
   return (
     <React.Fragment>
       {
+        user?.logo_url ?
+          <img
+            src={user.logo_url}
+            className={logoClassName || 'user-img-small'}
+            style={iconStyle}
+          /> :
         (authenticated || isLoggedIn()) ?
-          (
-            user?.logo_url ?
-              <img src={user.logo_url} className={logoClassName || 'user-img-small'} style={iconStyle} /> :
-            <PersonIcon color={color} sx={iconStyle} />
-          ) :
-          <StrangerIcon color={color} sx={iconStyle} />
+          <PersonIcon color={color} sx={iconStyle} /> :
+        <StrangerIcon color={color} sx={iconStyle} />
       }
     </React.Fragment>
   )
