f6430aae44c40b3afe52959ee0ed02e02f65bd30
Sny <sny.aggarwal@gmail.com>
2024-10-15 16:18:56 +0530

    Errbit | fixing typo

--- src/components/repos/VersionMeta.jsx
+++ src/components/repos/VersionMeta.jsx
@@ -9,7 +9,7 @@ import TableRow from '@mui/material/TableRow';
 import Skeleton from '@mui/material/Skeleton';
 import get from 'lodash/get'
 import RepoVersionLabel from './RepoVersionLabel'
-import { formatDate } from '../../common'
+import { formatDate } from '../../common/utils'
 
 const StatRow = ({icon, label, version1, version2, statKey, statFunc}) => {
   const lastCellStyle = {borderBottom: '1px solid', borderColor: 'surface.nv80'}
