03aa67956f0b7fd11724e9e608056fb5b52c7f8f
Sny <sny.aggarwal@gmail.com>
2024-12-10 08:07:58 +0530

    Repo Header | minimal view

--- src/components/repos/RepoHeader.jsx
+++ src/components/repos/RepoHeader.jsx
@@ -15,7 +15,7 @@ import RepoManagementList from './RepoManagementList';
 import FollowActionButton from '../common/FollowActionButton'
 import EntityAttributesDialog from '../common/EntityAttributesDialog'
 
-const RepoHeader = ({repo, owner, versions, onVersionChange, onCreateConceptClick}) => {
+const RepoHeader = ({repo, owner, versions, onVersionChange, onCreateConceptClick, essentials}) => {
   const { t } = useTranslation()
   const [menu, setMenu] = React.useState(false)
   const [menuAnchorEl, setMenuAnchorEl] = React.useState(false)
@@ -50,86 +50,91 @@ const RepoHeader = ({repo, owner, versions, onVersionChange, onCreateConceptClic
     <Paper component="div" className='col-xs-12' sx={{backgroundColor: 'surface.main', boxShadow: 'none', padding: '16px', borderRadius: '10px 10px 0 0'}}>
       <div className='col-xs-12 padding-0' style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', overflow: 'auto'}}>
         <span style={{display: 'flex', alignItems: 'center'}}>
-        <OwnerChip owner={owner} sx={{background: 'transparent', borderColor: 'surface.light'}} hideType />
-        <RepoChip repo={repo} sx={{marginLeft: '12px', background: 'transparent', borderColor: 'surface.light'}} onChange={onVersionChange}  />
-        {
-          onVersionChange &&
-            <RepoVersionChip checkbox version={repo} versions={versions} sx={{marginLeft: '8px', borderRadius: '4px'}} onChange={onVersionChange} />
-        }
-      </span>
+          <OwnerChip owner={owner} sx={{background: 'transparent', borderColor: 'surface.light'}} hideType />
+          <RepoChip repo={repo} sx={{marginLeft: '12px', background: 'transparent', borderColor: 'surface.light'}} onChange={onVersionChange}  />
+          {
+            onVersionChange &&
+              <RepoVersionChip checkbox version={repo} versions={versions} sx={{marginLeft: '8px', borderRadius: '4px'}} onChange={onVersionChange} />
+          }
+        </span>
         <span style={{display: 'flex', alignItems: 'center', marginLeft: '16px'}}>
-        <FollowActionButton iconButton entity={repo} />
-        {
-          Boolean(hasAccess && repo.version === 'HEAD' && has(repo, 'source_type')) &&
-            <React.Fragment>
-              <Button endIcon={<DownIcon fontSize='inherit' />} variant='text' sx={{textTransform: 'none', color: 'surface.contrastText'}} onClick={onMenuOpen} id='repo-manage'>
-                {t('repo.manage')}
-              </Button>
-              <RepoManagementList anchorEl={menuAnchorEl} open={menu} onClose={onMenuClose} id='repo-manage' onClick={onManageOptionClick} />
-          </React.Fragment>
-        }
-          </span>
+          <FollowActionButton iconButton entity={repo} />
+          {
+            Boolean(hasAccess && repo.version === 'HEAD' && has(repo, 'source_type')) &&
+              <React.Fragment>
+                <Button endIcon={<DownIcon fontSize='inherit' />} variant='text' sx={{textTransform: 'none', color: 'surface.contrastText'}} onClick={onMenuOpen} id='repo-manage'>
+                  {t('repo.manage')}
+                </Button>
+                <RepoManagementList anchorEl={menuAnchorEl} open={menu} onClose={onMenuClose} id='repo-manage' onClick={onManageOptionClick} />
+              </React.Fragment>
+          }
+        </span>
       </div>
-      <div className='col-xs-12 padding-0' style={{margin: '8px 0'}}>
+      <div className='col-xs-12 padding-0' style={{margin: '8px 0', marginBottom: essentials ? 0 : '8px'}}>
         <Typography component='span' sx={{fontSize: '28px', color: 'surface.dark', fontWeight: 600}}>{repo.name}</Typography>
         {
           repo?.canonical_url &&
             <Typography component='span' sx={{marginLeft: '8px', fontSize: '14px', color: 'secondary.main'}}>{repo.canonical_url}</Typography>
         }
       </div>
-      <div className='col-xs-12 padding-0' style={{display: 'flex', alignItems: 'center', fontSize: '16px'}}>
-        <span style={{display: 'flex', alignItems: 'center'}}>
-          <a style={{color: PRIMARY_COLORS.main, cursor: 'pointer'}} className='no-anchor-styles' onClick={() => setViewAll(true)}>{t('common.view_all_attributes')}</a>
-        </span>
-        <DotSeparator margin="0 6px" />
-        <span style={{display: 'flex', alignItems: 'center', opacity: 0.7}}>
-          {t('common.updated_on')} {formatDate(repo.updated_on)}
-        </span>
-      </div>
-      <EntityAttributesDialog
-        fields={{
-          name: {label: t('common.name')},
-          full_name: {label: t('common.full_name')},
-          external_id: {label: t('common.external_id')},
-          repo_type: {label: t('repo.repo_type')},
-          description: {label: t('common.description')},
-          canonical_url: {label: t('url_registry.canonical_url')},
-          locales: {label: t('repo.locales')},
-          custom_validation_schema: {label: t('repo.custom_validation_schema')},
-          public_access: {label: t('common.access_level')},
-          identifier: {label: t('repo.identifier'), type: 'json'},
-          contact: {label: t('repo.contact'), type: 'json'},
-          jurisdiction: {label: t('repo.jurisdiction'), type: 'json'},
-          publisher: {label: t('repo.publisher')},
-          purpose: {label: t('repo.purpose')},
-          copyright: {label: t('repo.copyright')},
-          content_type: {label: t('repo.content_type')},
-          revision_date: {label: t('repo.revision_date'), type: 'date'},
-          experimental: {label: t('repo.experimental')},
-          case_sensitive: {label: t('repo.case_sensitive')},
-          hierarchy_meaning: {label: t('repo.hierarchy_meaning')},
-          compositional: {label: t('repo.compositional')},
-          version_needed: {label: t('repo.version_needed')},
-          autoid_concept_mnemonic: {label: t('repo.autoid_concept_mnemonic')},
-          autoid_concept_external_id: {label: t('repo.autoid_concept_external_id')},
-          autoid_concept_name_external_id: {label: t('repo.autoid_concept_name_external_id')},
-          autoid_concept_description_external_id: {label: t('repo.autoid_concept_description_external_id')},
-          autoid_mapping_mnemonic: {label: t('repo.autoid_mapping_mnemonic')},
-          autoid_mapping_external_id: {label: t('repo.autoid_mapping_external_id')},
-          'checksums.standard': {label: t('checksums.standard')},
-          'checksums.smart': {label: t('checksums.smart')},
-          extras: {label: t('custom_attributes.label'), type: 'json'},
-          created_on: {label: t('common.created_on'), type: 'datetime'},
-          updated_on: {label: t('common.updated_on'), type: 'datetime'},
-          created_by: {label: t('common.created_by'), type: 'user'},
-          updated_by: {label: t('common.updated_by'), type: 'user'},
-        }}
-        entity={getRepo()}
-        open={viewAll}
-        onClose={() => setViewAll(false)}
-      />
+      {
+        !essentials &&
+          <>
+            <div className='col-xs-12 padding-0' style={{display: 'flex', alignItems: 'center', fontSize: '16px'}}>
+              <span style={{display: 'flex', alignItems: 'center'}}>
+                <a style={{color: PRIMARY_COLORS.main, cursor: 'pointer'}} className='no-anchor-styles' onClick={() => setViewAll(true)}>{t('common.view_all_attributes')}</a>
+              </span>
+              <DotSeparator margin="0 6px" />
+              <span style={{display: 'flex', alignItems: 'center', opacity: 0.7}}>
+                {t('common.updated_on')} {formatDate(repo.updated_on)}
+              </span>
+            </div>
+            <EntityAttributesDialog
+              fields={{
+                name: {label: t('common.name')},
+                full_name: {label: t('common.full_name')},
+                external_id: {label: t('common.external_id')},
+                repo_type: {label: t('repo.repo_type')},
+                description: {label: t('common.description')},
+                canonical_url: {label: t('url_registry.canonical_url')},
+                locales: {label: t('repo.locales')},
+                custom_validation_schema: {label: t('repo.custom_validation_schema')},
+                public_access: {label: t('common.access_level')},
+                identifier: {label: t('repo.identifier'), type: 'json'},
+                contact: {label: t('repo.contact'), type: 'json'},
+                jurisdiction: {label: t('repo.jurisdiction'), type: 'json'},
+                publisher: {label: t('repo.publisher')},
+                purpose: {label: t('repo.purpose')},
+                copyright: {label: t('repo.copyright')},
+                content_type: {label: t('repo.content_type')},
+                revision_date: {label: t('repo.revision_date'), type: 'date'},
+                experimental: {label: t('repo.experimental')},
+                case_sensitive: {label: t('repo.case_sensitive')},
+                hierarchy_meaning: {label: t('repo.hierarchy_meaning')},
+                compositional: {label: t('repo.compositional')},
+                version_needed: {label: t('repo.version_needed')},
+                autoid_concept_mnemonic: {label: t('repo.autoid_concept_mnemonic')},
+                autoid_concept_external_id: {label: t('repo.autoid_concept_external_id')},
+                autoid_concept_name_external_id: {label: t('repo.autoid_concept_name_external_id')},
+                autoid_concept_description_external_id: {label: t('repo.autoid_concept_description_external_id')},
+                autoid_mapping_mnemonic: {label: t('repo.autoid_mapping_mnemonic')},
+                autoid_mapping_external_id: {label: t('repo.autoid_mapping_external_id')},
+                'checksums.standard': {label: t('checksums.standard')},
+                'checksums.smart': {label: t('checksums.smart')},
+                extras: {label: t('custom_attributes.label'), type: 'json'},
+                created_on: {label: t('common.created_on'), type: 'datetime'},
+                updated_on: {label: t('common.updated_on'), type: 'datetime'},
+                created_by: {label: t('common.created_by'), type: 'user'},
+                updated_by: {label: t('common.updated_by'), type: 'user'},
+              }}
+              entity={getRepo()}
+              open={viewAll}
+              onClose={() => setViewAll(false)}
+            />
+          </>
+      }
     </Paper>
-  )
+)
 }
 
 export default RepoHeader;
