d4588480345fd370a0d5c0d0a70f79e629693578
Sny <sny.aggarwal@gmail.com>
2025-05-05 14:14:50 +0530

    OpenConceptLab/ocl_issues#1616 | user menu to add repo/org

--- src/components/app/HeaderControls.jsx
+++ src/components/app/HeaderControls.jsx
@@ -1,14 +1,42 @@
 import React from 'react';
+import IconButton from '@mui/material/IconButton'
+import AddIcon from '@mui/icons-material/Add'
 import UserMenu from '../users/UserMenu';
 import UserProfileButton from '../users/UserProfileButton';
+import AddMenuList from '../users/AddMenuList'
+import { isLoggedIn } from '../../common/utils';
 
 
 const HeaderControls = () => {
   const [userMenu, setUserMenu] = React.useState(false)
+  const [addMenu, setAddMenu] = React.useState(false)
+  const [addMenuAnchorEl, setAddMenuAnchorEl] = React.useState(false)
+
+  const onAddMenuOpen = event => {
+    setAddMenuAnchorEl(event.currentTarget)
+    setAddMenu(true)
+  }
+  const onAddMenuClose = () => {
+    setAddMenuAnchorEl(false)
+    setAddMenu(false)
+  }
+
+  const authenticated = isLoggedIn()
+
   return (
     <div className='col-xs-3 padding-0' style={{textAlign: 'right'}}>
+      {
+        authenticated &&
+          <IconButton sx={{marginRight: '8px'}} onClick={onAddMenuOpen}>
+            <AddIcon />
+          </IconButton>
+      }
       <UserProfileButton onClick={() => setUserMenu(true)} />
       <UserMenu isOpen={userMenu} onClose={() => setUserMenu(false)} />
+      {
+        authenticated &&
+          <AddMenuList open={addMenu} onClose={onAddMenuClose} anchorEl={addMenuAnchorEl} />
+      }
     </div>
   )
 }
---
+++ src/components/users/AddMenuList.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next'
+import { Menu, ListItemButton, ListItemText, ListItemIcon} from '@mui/material'
+import { getCurrentUser } from '../../common/utils';
+import OrgIcon from '../orgs/OrgIcon'
+import RepoIcon from '../repos/RepoIcon'
+
+const AddMenuList = ({ anchorEl, open, onClose }) => {
+  const { t } = useTranslation()
+  const user = getCurrentUser()
+  return (
+    <Menu
+      id='user-add-menu-list'
+      anchorEl={anchorEl}
+      open={open}
+      onClose={onClose}
+      sx={{'.MuiPaper-root': {backgroundColor: 'surface.n94', minWidth: '200px'}}}
+    >
+      <ListItemButton id='addOrg' href={`#${user?.url}orgs/new`} sx={{padding: '8px 12px', '&:hover': {color: 'inherit'}, '&:focus': {outline: 'none', textDecoration: 'none', color: 'inherit'}}} onClick={onClose}>
+        <ListItemIcon sx={{minWidth: 'auto', marginRight: '12px'}}>
+          <OrgIcon noTooltip noLink strict />
+        </ListItemIcon>
+        <ListItemText primary={t('org.org')} />
+      </ListItemButton>
+      <ListItemButton id='addRepo' href={`#${user?.url}repos/new`} sx={{padding: '8px 12px', '&:hover': {color: 'inherit'}, '&:focus': {outline: 'none', textDecoration: 'none', color: 'inherit'}}} onClick={onClose}>
+        <ListItemIcon sx={{minWidth: 'auto', marginRight: '12px'}}>
+          <RepoIcon noTooltip />
+        </ListItemIcon>
+        <ListItemText primary={t('repo.repo')} />
+      </ListItemButton>
+    </Menu>
+  )
+}
+
+export default AddMenuList;
