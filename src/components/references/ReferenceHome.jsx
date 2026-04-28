c4ac2199914f726a9cf0bcad184c0f8fb885a02b
Sunny Aggarwal <sny.aggarwal@gmail.com>
2026-04-28 11:38:32 +0530

    OpenConceptLab/ocl_issues#2123 | fixing ref change when loaded

--- src/components/references/ReferenceHome.jsx
+++ src/components/references/ReferenceHome.jsx
@@ -15,54 +15,69 @@ const ReferenceHome = props => {
   const [conceptHeaders, setConceptHeaders] = React.useState(false)
   const [mappings, setMappings] = React.useState(false)
   const [mappingHeaders, setMappingHeaders] = React.useState(false)
+  const activeReferenceIdRef = React.useRef(reference?.id)
 
   const repoURL = props?.repo?.version_url || props?.repo?.url
 
-  React.useEffect(() => {
+  const resetExpansionState = () => {
+    setLoading(false)
     setConcepts(false)
     setMappings(false)
     setConceptHeaders(false)
     setMappingHeaders(false)
-    if(tab === 'expansion')
-      setTimeout(() => getResults(true), 100)
+  }
+
+  React.useEffect(() => {
+    activeReferenceIdRef.current = reference?.id
+    resetExpansionState()
   }, [reference?.id])
+
+  React.useEffect(() => {
+    if(tab === 'expansion')
+      getResults({ force: true, reset: true, currentReferenceId: reference?.id })
+  }, [reference?.id, tab])
 
   const onTabChange = newTab => {
     setTab(newTab)
-    if(newTab === 'expansion')
-      getResults()
   }
-  const getResults = (force=false) => {
+
+  const getResults = ({ force=false, reset=false, currentReferenceId=reference?.id } = {}) => {
     if(reference.reference_type === 'mappings' && (force || !mappings?.length)) {
-      fetchMappings()
+      fetchMappings({ reset, currentReferenceId })
     }
     else if (force || !concepts?.length) {
-      fetchConcepts()
+      fetchConcepts({ reset, currentReferenceId })
     }
   }
   const getRefService = () => APIService.new().overrideURL(repoURL).appendToUrl(`references/${reference.id}/`)
 
-  const fetchConcepts = () => {
-    const { limit, page } = getLimits(conceptHeaders)
+  const fetchConcepts = ({ reset=false, currentReferenceId=reference?.id } = {}) => {
+    const { limit, page } = getLimits(reset ? false : conceptHeaders)
     if(limit === 0)
       return
 
     setLoading(true)
     getRefService().appendToUrl('concepts/').get(null, null, {limit: limit, page: page, includeMappings: true, mappingBrief: true}).then(response => {
-      setConcepts(page === 1 ? response.data : [...(concepts || []), ...response.data])
+      if(activeReferenceIdRef.current !== currentReferenceId)
+        return
+
+      setConcepts(currentConcepts => (page === 1 ? response.data : [...(currentConcepts || []), ...response.data]))
       setConceptHeaders(response.headers)
       setLoading(false)
     })
   }
 
-  const fetchMappings = () => {
-    const { limit, page } = getLimits(mappingHeaders)
+  const fetchMappings = ({ reset=false, currentReferenceId=reference?.id } = {}) => {
+    const { limit, page } = getLimits(reset ? false : mappingHeaders)
     if(limit === 0)
       return
 
     setLoading(true)
     getRefService().appendToUrl('mappings/').get(null, null, {limit: limit, page: page}).then(response => {
-      setMappings(page === 1 ? response.data : [...(mappings || []), ...response.data])
+      if(activeReferenceIdRef.current !== currentReferenceId)
+        return
+
+      setMappings(currentMappings => (page === 1 ? response.data : [...(currentMappings || []), ...response.data]))
       setMappingHeaders(response.headers)
       setLoading(false)
     })
