70b4d76cb27cb1027af7b0792881274db64f976b
Joseph Amlung <jamlung@regenstrief.org>
2026-05-05 09:05:11 -0400

    First try

--- src/common/plugins/loader.js
+++ src/common/plugins/loader.js
@@ -2,7 +2,7 @@ export async function loadUsageDashboard() {
   try {
     // webpack won't try to resolve this at build time
     /*eslint no-undef: 0*/
-    const mod = await import("ocl-analytics-web");
+    const mod = await import(/* webpackIgnore: true */ "ocl-analytics-web");
     /*eslint no-console: 0*/
     return mod.UsageDashboard || mod.default || null;
   } catch (e) {
